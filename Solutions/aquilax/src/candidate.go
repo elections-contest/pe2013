5953d0e0f89d958d24cb895994a5035841fe10e8
Evgeniy <evgeniy@boxore.com>
2013-04-24 10:11:14 +0300

    Added missing field in candidates

--- Solutions/aquilax/src/candidate.go
+++ Solutions/aquilax/src/candidate.go
@@ -5,9 +5,10 @@ import (
 )
 
 type Candidate struct {
-	mir_id   int
-	party_id int
-	name     string
+	mir_id        int
+	party_id      int
+	candidate_num int
+	name          string
 }
 
 type Candidates []Candidate
@@ -19,7 +20,8 @@ func NewCandidates() Candidates {
 func (c Candidates) Add(record []string) {
 	mir_id, _ := strconv.Atoi(record[0])
 	party_id, _ := strconv.Atoi(record[1])
-	c = append(c, Candidate{mir_id, party_id, record[2]})
+	candidate_num, _ := strconv.Atoi(record[2])
+	c = append(c, Candidate{mir_id, party_id, candidate_num, record[3]})
 }
 
 func (c Candidates) Load(path string) {
